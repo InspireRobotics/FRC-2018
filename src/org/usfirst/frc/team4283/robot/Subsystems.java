a8170cf7c1acee08c92c0920c40ddf802d22e591
DevOrc <ncharlton002@gmail.com>
2018-01-30 15:17:22 -0500

    Add more stuff

--- src/org/usfirst/frc/team4283/robot/Subsystems.java
+++ src/org/usfirst/frc/team4283/robot/Subsystems.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team4283.robot;
 
-public class Subsystems {
+import org.usfirst.frc.team4283.robot.drivetrain.Drivetrain;
+import org.usfirst.frc.team4283.robot.intake.Intake;
 
+public class Subsystems {
+	
+	public static final Drivetrain DRIVE = new Drivetrain();
+	public static final Intake INTAKE = new Intake();
+	
 }
