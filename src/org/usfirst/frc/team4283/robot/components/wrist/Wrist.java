5a4d96316faa51b23c7f443796a9435c319a5cd6
DevOrc <ncharlton002@gmail.com>
2018-02-01 16:20:15 -0500

    Updated Talon Code

--- src/org/usfirst/frc/team4283/robot/components/wrist/Wrist.java
+++ src/org/usfirst/frc/team4283/robot/components/wrist/Wrist.java
@@ -3,13 +3,20 @@ package org.usfirst.frc.team4283.robot.components.wrist;
 import org.usfirst.frc.team4283.robot.HardwareMap;
 import org.usfirst.frc.team4283.robot.Subsystems;
 import org.usfirst.frc.team4283.robot.components.Component;
+import org.usfirst.frc.team4283.robot.util.SmartDouble;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Wrist extends Subsystem implements Component {
+	
+	/*
+	 * PID Values
+	 */
+	private static final SmartDouble P = new SmartDouble(0.005, "WRIST P"),  I = new SmartDouble(0, "WRIST I"), D = new SmartDouble(0, ""), F = 0;
 	
 	private TalonSRX wrist;
 	
@@ -23,6 +30,11 @@ public class Wrist extends Subsystem implements Component {
 	@Override
 	public void teleOpInit() {
 		wrist.set(ControlMode.PercentOutput, 0);
+		
+		wrist.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+		wrist.config_kP(0, P.get(), 0);
+		wrist.config_kI(0, I.get(), 0);
+		wrist.config_kD(0, D.get(), 0);
 	}
 
 	@Override
